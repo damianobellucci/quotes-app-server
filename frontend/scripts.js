fe79789c50806c81098949777dded098fe779bae
damianobellucci <belluccidamiano@gmail.com>
2022-05-30 15:37:45 +0200

    update

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -7,26 +7,8 @@ function GetQuote(){
 
             data = data.quotes[0]
 
-            /*remove present quote*/
-            document.getElementById('quotes').innerHTML = '';
+            document.getElementById('text-quote').innerHTML = data.quote;
+            document.getElementById('author-info').innerHTML = "- " + data.author;
 
-
-            var element = document.createElement("div");
-
-            content = '<p>'
-                      +
-                      data.quote
-                      +
-                      '</p>'
-                      +
-                      '<div class=info-author>'
-                      + data.author 
-                      + ',   '
-                      +data.info
-                      +
-                      '</div>'
-
-            element.innerHTML = content
-            document.getElementById('quotes').appendChild(element);
         })
 }

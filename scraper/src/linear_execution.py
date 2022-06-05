3f8e9fbec98e32e2ed798aa6f7324e2a6bc8e794
damianobellucci <belluccidamiano@gmail.com>
2022-06-05 12:05:39 +0200

    commit

--- scraper/src/linear_execution.py
+++
@@ -1,9 +0,0 @@
-import parsing_functions
-
-
-def linear_execution(authors):
-    count = len(authors)
-    for author in authors:
-        print("remaining authors: " + str(count))
-        parsing_functions.atomic_operation(author)
-        count -= 1

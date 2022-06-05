56514b35444ab877a24e58c4c0e3d82fdc944c97
damianobellucci <belluccidamiano@gmail.com>
2022-06-05 12:53:37 +0200

    update

--- application.py
+++ application.py
@@ -6,14 +6,6 @@ from uuid import uuid4
 app = Flask(__name__)
 CORS(app)
 
-#sqlite connector
-def get_db_connection():
-    conn = sqlite3.connect('quotes_app.db')
-    conn.row_factory = sqlite3.Row
-    return conn
-
-# get a number of quotes specified bu the request arg "number"
-# example: http://127.0.0.1:5000/random_quote?number=3 returns a JSON object with a list of three random quotes
 
 @app.route("/ping", methods=['GET'])
 def ping():
